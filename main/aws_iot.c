f51cd7d83323251363a11784023a5d303aa6cf50
Ward Coessens <coessens@amazon.com>
2022-12-07 14:17:07 +0100

    working taking picture

--- main/aws_iot.c
+++ main/aws_iot.c
@@ -31,14 +31,14 @@
 
 #include "mqtt_client.h"
 
+
 #include "esp_camera.h"
 
-// Camera connection pins, found : https://www.uctronics.com/download/esp32-cam-board-specs.pdf
-#define CAM_PIN_PWDN    -1 //power down is not used
+#define CAM_PIN_PWDN    32 
 #define CAM_PIN_RESET   -1 //software reset will be performed
-#define CAM_PIN_XCLK     0
-#define CAM_PIN_SIOD    26 //SDA
-#define CAM_PIN_SIOC    27 //SCL
+#define CAM_PIN_XCLK    0
+#define CAM_PIN_SIOD    26
+#define CAM_PIN_SIOC    27
 
 #define CAM_PIN_D7      35
 #define CAM_PIN_D6      34
@@ -51,9 +51,6 @@
 #define CAM_PIN_VSYNC   25
 #define CAM_PIN_HREF    23
 #define CAM_PIN_PCLK    22
-
-
-
 
 
 static camera_config_t camera_config = {
@@ -75,12 +72,14 @@ static camera_config_t camera_config = {
     .pin_href = CAM_PIN_HREF,
     .pin_pclk = CAM_PIN_PCLK,
 
-    .xclk_freq_hz = 20000000,//EXPERIMENTAL: Set to 16MHz on ESP32-S2 or ESP32-S3 to enable EDMA mode
+    .xclk_freq_hz = 10000000,//EXPERIMENTAL: Set to 16MHz on ESP32-S2 or ESP32-S3 to enable EDMA mode
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
 
-    .pixel_format = PIXFORMAT_JPEG,//YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_UXGA,//QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
+    .pixel_format = PIXFORMAT_RGB565,//YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_96X96,//QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
+
+    .fb_location = CAMERA_FB_IN_DRAM, // No PSRAM
 
     .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
     .fb_count = 1, //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
@@ -109,6 +108,19 @@ static void log_error_if_nonzero(const char *message, int error_code)
     }
 }
 
+void upload_image(camera_fb_t * fb){
+    ESP_LOGI(TAG, "About to upload image");
+
+    uint8_t * buf = NULL;
+    size_t buf_len = 0;
+    bool converted = frame2bmp(fb, &buf, &buf_len);
+
+    // if(converted){
+    //     esp_mqtt_client_publish(client, "/device/photos", (char *) buf, &buf_len, 0, 0);
+    // }else{
+    //     ESP_LOGE(TAG, "Camera Init Failed");
+    // }
+}
 
 
 esp_err_t camera_init(){
@@ -131,7 +143,7 @@ esp_err_t camera_capture(){
         return ESP_FAIL;
     }
     //replace this with your own function
-    //process_image(fb->width, fb->height, fb->format, fb->buf, fb->len);
+    upload_image(fb);
   
     //return the frame buffer back to the driver for reuse
     esp_camera_fb_return(fb);
